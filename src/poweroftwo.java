f8d14edd189ffff611cc922e2590b72c94c225e6
abhishekgaikwad10 <abhishekgaikwad326@gmail.com>
2021-09-09 15:27:09 +0530

    power of two

---
+++ src/poweroftwo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class poweroftwo {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the N:");
+        int powerOfTwo = sc.nextInt();
+
+        if (powerOfTwo <= 0 || powerOfTwo < 31) {
+            System.out.println("Power of 2's: ");
+
+            for (int i = 0; i <= powerOfTwo; i++) {
+                int powerOfValue = ((int) Math.pow(2, i));      //2^i
+                System.out.println("The value of 2^" + i + " is: " + powerOfValue);
+            }
+        } else
+            System.out.println("Value overflows int value");
+
+
+    }
+
+}
